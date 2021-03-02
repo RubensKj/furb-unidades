5650354c5a86eda740443cd1399af6a4b820aa11
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-02 19:29:16 -0300

    Adding Unidade1

---
+++ Unidade1/src/Exercicio1.java
@@ -0,0 +1,16 @@
+public class Exercicio1 {
+    public static void main(String[] args) {
+        double nota1 = 7;
+        double nota2 = 5;
+        double nota3 = 1.5;
+
+        String nomeCompleto = "Rubens Kleinschmidt Jr";
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println(nomeCompleto);
+        System.out.println(media);
+
+        System.out.println("Aluno(a): " + nomeCompleto + " possui a média " + media);
+    }
+}
---
+++ Unidade1/src/Exercicio2.java
@@ -0,0 +1,11 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        double lado1 = 10;
+        double lado2 = 10;
+
+        double area = lado1 * lado2;
+
+        System.out.println("A área é: " + area + " m2");
+    }
+}
