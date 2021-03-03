2039f85a2eb8223759dc336eece1b523e1244851
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-02 21:24:22 -0300

    Creating challenge given by the teacher

---
+++ Unidade2/src/Desafio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Desafio1 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        double valorA = teclado.nextDouble();
+
+        double valorAMultiplicado = valorA * 3.5;
+
+        System.out.print("Digite um segundo número: ");
+        double valorB = teclado.nextDouble();
+
+        double valorBMultiplicado = valorB * 7.5;
+
+        double soma = (valorAMultiplicado + valorBMultiplicado) / 11;
+
+        System.out.println("MEDIA = " + soma);
+    }
+}
