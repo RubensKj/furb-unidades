5a39e4691187cf43c9792364e50034616a8ac54f
rubenskj <rubenskleinschmidtjr@gmail.com>
2021-03-04 21:48:44 -0300

    Exercise from class for today

---
+++ Unidade2/src/Problema5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Problema5 {
+    public static void main(String[] args) {
+
+        // Análise: OK
+
+        // Oq nao é necessario: ..faça um programa que calcule e mostre.. | ..dentre elas o Java/C++..
+
+        // Entradas:
+        // raio
+
+        // Saidas:
+        // Volume de uma esfera
+
+        // Testes:
+        // raio / volume
+        // 3 / 113.09724
+        // 2 / 33.5102933
+
+        // Algoritmo:
+
+        double pi = 3.14159;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe o raio da esfera: ");
+        double raio = entrada.nextDouble();
+
+        double volume = (4.0 / 3) * pi * Math.pow(raio, 3);
+
+        System.out.println("O volume da esfera é: " + volume);
+
+    }
+}
